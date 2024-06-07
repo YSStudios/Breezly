53d9385d219d4c345e6a035a9ca39cc8365bd9ca
Sina <visualsbysina@gmail.com>
2024-06-07 15:18:47 -0400

    Update sign-in-modal.tsx applogo

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -23,14 +23,14 @@ const SignInModal = ({
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
       <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
-            />
+          <a href="https://offerapp.vercel.app">
+		  <Image
+              src="/applogo.webp"
+              alt="OfferApp"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            ></Image>
           </a>
           <h3 className="font-display text-2xl font-bold">Sign In</h3>
           <p className="text-sm text-gray-500">
