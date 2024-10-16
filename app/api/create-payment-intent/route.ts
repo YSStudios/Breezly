f3d3d67eee6483bcf38adf03bef5b5460e5a2851
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2024-10-16 16:27:13 -0400

    fix api version

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
-});
+} as unknown as Stripe.StripeConfig);
 
 export async function POST(request: Request) {
   try {
