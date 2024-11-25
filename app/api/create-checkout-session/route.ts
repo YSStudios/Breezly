f6fe815fa7185a63c11d4771dd008e3c5efda81f
Sina <visualsbysina@gmail.com>
2024-11-24 23:52:21 -0500

    fix stripe apiversion

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/options";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-09-30.acacia",
+  apiVersion: "2024-11-20.acacia",
 });
 
 export async function POST(req: Request) {
